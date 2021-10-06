521b4ac65ebd524740e9d27567554224d6a71eaf
MichaelWeih <michaelweih.mw@gmail.com>
2021-10-06 15:03:42 +0200

    Struktur verbessert

--- SpaceGame/Javascript/Ammo.js
+++ SpaceGame/Javascript/Ammo.js
@@ -7,3 +7,37 @@ function resetAmmo(){
     ammo.currentMag = ammo.standardMag;
     document.getElementById('ammo').innerHTML = `${ammo.currentMag}`;
 }
+
+function createBullets(){
+    if(isKEY_E){
+        if  (KEY_E && !rocket.hit)  {
+            let bullet = {
+                x: rocket.x + 80,
+                y: rocket.y + 22,
+                width: 40,
+                height: 10,
+                src: 'Images/bullet.png',
+                img: new Image()
+            };
+            bullet.img.src = bullet.src;
+            bullets.push(bullet);
+        }
+    }
+}
+
+function createInvertedBullets(){
+    if(isKEY_Q){
+        if  (KEY_Q && !rocket.hit){
+            let invertedBullet = {
+                x: rocket.x - 80,
+                y: rocket.y + 22,
+                width: 40,
+                height: 10,
+                src: 'Images/bullet.png',
+                img: new Image()
+            };
+            invertedBullet.img.src = invertedBullet.src;
+            invertedBullets.push(invertedBullet);
+        }
+    }
+}
